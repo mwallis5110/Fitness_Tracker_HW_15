b5cbc83310b00742753ef4573c477976925e9617
Mason <mwallis5110@gmail.com>
2021-08-16 12:39:13 -0500

    Updating api routes

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,21 +1,50 @@
 const router = require("express").Router();
-
+const Workouts = require("../../models/workouts.js")
 const path = require("path");
 
 router.get("/api/workouts", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/workout.js"));
+  Workouts.findOne({})
+    .sort({day: -1}) //Day or date?
+    .then(Workouts => {
+      res.json(Workouts);
+    })
+    .catch(err => {
+      res.status("api/workouts GET route not working").json(err)
+    });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/workout.js"));
+  Workouts.findById(req.params.id)
+    .then(Workouts => {
+      res.json(Workouts);
+    })
+
+    .catch(err => {
+      res.status("api/workouts/:id PUT route not working").json(err)
+  });
 });
 
-router.post("/api/workouts", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/workout.js"));
-});
+// router.post("/api/workouts", (req, res) => {
+//    Workouts.insertMany()
+//      .then((Workouts) => {
+//        res.json(Workouts);
+//      })
 
-router.get("/api/workouts/range", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/workout.js"));
+//      .catch((err) => {
+//        res.status("api/workouts POST route not working").json(err);
+//      });
+// });
+
+router.get("/api/workouts/range", (req, res) => { //Where is 'range' coming from?
+
+  // Workouts.find({})
+//    .sort({ })
+//     .then(Workouts => {
+//       res.json(Workouts);
+//     })
+//     .catch(err => {
+//       res.status("api/workouts/range GET route not working").json(err)
+//     });
 });
 
 module.exports = router;
