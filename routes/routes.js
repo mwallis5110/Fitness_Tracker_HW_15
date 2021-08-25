32fb88700bc02e2f9f9a16630c909b8d17d1530e
Mason Wallis <mwallis5110@gmail.com>
2021-08-25 15:32:12 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -76,15 +76,15 @@ router.get("/api/workouts/range", (req, res) => {
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/stats.html"));
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
